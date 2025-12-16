77b689caa843352adc2f696ffe5026cb30454f3a
Caetano AG <caetanogoncalves.alves@gmail.com>
2025-12-16 12:57:10 -0300

    Sistema de restaurantes adicionado

--- server.js
+++ server.js
@@ -158,6 +158,55 @@ app.get('/farmacias/:cep', async (req, res) =>{
         });
     }
 });
+app.get('/restaurantes/:cep', async (req, res) =>{
+
+    try{let cepRequest = req.params.cep;
+    cepRequest = cepRequest.replace(/\D/g, '');
+    if (cepRequest.length !== 8) {
+        return res.status(400).json({
+            status:"error",
+            erro: "CEP Inválido",
+            mensagem: "O CEP deve conter exatamente 8 números."
+        });
+    }
+    const data = await cepLocator(cepRequest);
+    const lon = data.gps.lon;
+    const lat = data.gps.lat;
+    const url = 'https://overpass-api.de/api/interpreter';
+    const query = `
+            [out:json];
+            (
+              node["amenity"="restaurant"](around:5000, ${lat}, ${lon});
+              way["amenity"="restaurant"](around:5000, ${lat}, ${lon});
+              relation["amenity"="restaurant"](around:5000, ${lat}, ${lon});
+            );
+            out center;
+        `;
+    const response = await axios.get(url, {
+        params: {data:query}
+    });
+    const restaurantes = response.data.elements.map(item => ({
+            nome: item.tags.name || "Nome indisponivel",
+            tipo: item.tags.cuisine || "Variado",
+            lat: item.lat || item.center.lat,
+            lon: item.lon || item.center.lon,
+            distancia: GpsDistance(lat, lon, item.lat || item.center.lat, item.lon || item.center.lon) + " km"
+        }));
+    res.status(200).json({
+        status:"sucess",
+        centro: {lat, lon},
+        total_encontrado: restaurantes.length,
+        restaurantes: restaurantes
+    })}
+    catch(err){
+        console.error("Erro na busca");
+        res.status(500).json({
+        status:"error",
+        mensage:"erro na busca de restaurantes",
+        details:err.message
+        });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em: http://localhost:${PORT}`);
